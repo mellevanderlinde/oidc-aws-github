1e5eb718e6a44178d4b44f11ea96a11d6b654dc5
Melle <melle@git.com>
2024-08-23 22:29:37 +0200

    Add IAM role

--- lib/openid-connect-aws-github-stack.ts
+++ lib/openid-connect-aws-github-stack.ts
@@ -5,8 +5,21 @@ export class OpenidConnectAwsGithubStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new iam.CfnOIDCProvider(this, "GithubOidcProvider", {
+    const oidcProvider = new iam.CfnOIDCProvider(this, "GithubOidcProvider", {
       url: "https://token.actions.githubusercontent.com",
+    });
+
+    const conditions: iam.Conditions = {
+      StringEquals: {
+        "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+        "token.actions.githubusercontent.com:sub":
+          "repo:mellevanderlinde/openid-connect-aws-github:ref:refs/heads/non-existing-branch", // TODO change to main branch, first verify that this fails
+      },
+    };
+
+    new iam.Role(this, "GithubOidcRole", {
+      roleName: "github-oidc-role",
+      assumedBy: new iam.WebIdentityPrincipal(oidcProvider.ref, conditions),
     });
   }
 }
