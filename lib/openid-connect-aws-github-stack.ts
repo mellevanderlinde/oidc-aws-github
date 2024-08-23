62484aa3481228e1b38155e3d4e1dd0387baf4b3
Melle <melle@git.com>
2024-08-23 21:57:59 +0200

    Add OIDC Identity Provider for GitHub

--- lib/openid-connect-aws-github-stack.ts
+++ lib/openid-connect-aws-github-stack.ts
@@ -1,8 +1,12 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps, aws_iam as iam } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 export class OpenidConnectAwsGithubStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    new iam.CfnOIDCProvider(this, "GithubOidcProvider", {
+      url: "https://token.actions.githubusercontent.com",
+    });
   }
 }
