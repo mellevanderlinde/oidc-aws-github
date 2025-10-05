8ca519885ab47b475e3b235d1cfecf306d52d175
Melle <118454433+mellevanderlinde@users.noreply.github.com>
2025-10-05 12:03:44 +0200

    Add fine grained CDK tests

--- test/oidc-aws-github.test.ts
+++ test/oidc-aws-github.test.ts
@@ -1,11 +1,51 @@
-import { App } from "aws-cdk-lib";
+import { App, DefaultStackSynthesizer } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
-import { expect, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
 import { OidcAwsGithubStack } from "../lib/oidc-aws-github-stack";
 
-it("matches snapshot", () => {
-  const app = new App();
-  const stack = new OidcAwsGithubStack(app, "OidcAwsGithubStack");
-  const template = Template.fromStack(stack);
-  expect(template).toMatchSnapshot();
+describe("oidc stack", () => {
+  let template: Template;
+
+  beforeAll(() => {
+    const defaultStackSynthesizer = new DefaultStackSynthesizer({ generateBootstrapVersionRule: false });
+    const app = new App({ defaultStackSynthesizer });
+    const stack = new OidcAwsGithubStack(app, "OidcAwsGithubStack");
+    template = Template.fromStack(stack);
+  });
+
+  it("matches snapshot", () => {
+    expect(template).toMatchSnapshot();
+  });
+
+  it("has an OIDC Provider", () => {
+    template.hasResourceProperties("AWS::IAM::OIDCProvider", {
+      Url: "https://token.actions.githubusercontent.com",
+      ClientIdList: ["sts.amazonaws.com"],
+    });
+  });
+
+  it("has a role that trusts a GitHub repository branch", () => {
+    template.hasResourceProperties("AWS::IAM::Role", {
+      AssumeRolePolicyDocument: {
+        Statement: [
+          {
+            Action: "sts:AssumeRoleWithWebIdentity",
+            Condition: {
+              StringEquals: {
+                "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+                "token.actions.githubusercontent.com:sub": "repo:mellevanderlinde/oidc-aws-github:ref:refs/heads/main",
+              },
+            },
+            Effect: "Allow",
+            Principal: {
+              Federated: {
+                Ref: "GithubOidcProvider",
+              },
+            },
+          },
+        ],
+        Version: "2012-10-17",
+      },
+    });
+  });
 });
